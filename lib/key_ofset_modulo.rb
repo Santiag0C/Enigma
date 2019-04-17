ee6c317ba98e452aaef245a27b957dce1f977ce1
Santiago Caraveo <santiagocbrvg@hotmail.com>
2019-04-16 21:32:28 -0700

    adds last decript helpers

--- lib/key_ofset_modulo.rb
+++ lib/key_ofset_modulo.rb
@@ -1,4 +1,5 @@
 module OffsetKey
+
   def offsets#date## used in combiner
     date = Time.new
     date = date.strftime("%d%m%y")
@@ -6,7 +7,7 @@ module OffsetKey
     squdat = squdat.to_s
     return squdat[-4..-1]
   end
-  
+
   def keys #used in combiner
     ran = 4.times.map { rand(10) }
     a = 0, ran[0]
@@ -14,5 +15,45 @@ module OffsetKey
     c = ran[1],ran[2]
     d = ran[2],ran[3]
     @shift = [a, b, c, d]
+  end
+
+  def key_comb(argument_encry_decry)#used in conbiner
+    if argument_encry_decry == true
+      keys
+      comb = @shift.map {|x| x.join}
+      @key = comb_i = comb.map {|x| x.to_i}
+    else
+      @key = converter_key(argument_encry_decry)
+    end
+  end
+
+  def converter_key(argument_encry_decry)#helpr for key comb
+    argument_encry_decry.scan(/../).map do |x|
+      x.to_i
+    end
+  end
+  def converter_offset(argument_encry_decry)#helper for offser_comb
+    argument_encry_decry.scan(/./).map do |x|
+      x.to_i
+    end
+  end
+
+  def offsets_comb(argument_encry_decry)
+    if argument_encry_decry == true
+      @off = of_i = offsets.scan(/./).map {|e| e.to_i}
+    else
+      @off = converter_offset(argument_encry_decry)
+    end
+  end
+
+  def combiner#shifting
+    sa = 0
+    sum = []
+    while sa != 4
+      sum << (@key[sa] + @off[sa])
+      sa += 1
+    end
+    sum
+    #[-3,-27,-73,-20]
   end
 end
