58547c6277429e00a2fe1a0df870bc4e0c521230
Santiago Caraveo <santiagocbrvg@hotmail.com>
2019-04-16 21:34:18 -0700

    adds some fikes to the module

---
+++ lib/encryptor_decryptor_module.rb
@@ -0,0 +1,38 @@
+module Encryptor
+
+  def index_gatherer(input_in_array)
+    index_of_input = []
+    input_in_array.each do |let|
+      @alph.each do |alp|# _with_index do |val, index|
+          index_of_input << @alph.index(let)  if let == alp
+        end
+      end
+    index_of_input
+  end
+
+  def encryptor_decryptor(text_in_shifts)
+    count = 0
+    time = 0
+    arr = []
+    while time != text_in_shifts.length
+      if count == 0
+          arr << @alph.rotate(combiner[count])[text_in_shifts[time]]
+          count += 1
+          time += 1
+      elsif count == 1
+          arr << @alph.rotate(combiner[count])[text_in_shifts[time]]
+          count += 1
+          time += 1
+      elsif count == 2
+          arr << @alph.rotate(combiner[count])[text_in_shifts[time]]
+          count += 1
+          time += 1
+      elsif count == 3
+          arr << @alph.rotate(combiner[count])[text_in_shifts[time]]
+          count = 0
+          time += 1
+      end
+    end
+    arr
+  end
+end
